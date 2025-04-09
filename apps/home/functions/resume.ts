109626f3102f0236b5046ff054fb587df376ece0
Dahye <psst5491@naver.com>
2025-04-09 20:47:12 +0900

    🚧 set resume proxy

--- apps/home/functions/resume.ts
+++ apps/home/functions/resume.ts
@@ -1,9 +1,20 @@
+// apps/home/functions/resume.ts
 export const onRequest = async ({ request }: { request: Request }) => {
   const url = new URL(request.url);
-  const newPath = url.pathname.replace(/^\/resume/, "") || "/";
-  const target = `https://rhei-resume.pages.dev${newPath}${url.search}`;
 
-  return fetch(target, {
+  // HTML (entrypoint) 프록시: /resume 또는 /resume/1 같은
+  if (!url.pathname.startsWith("/resume/assets/")) {
+    const proxyURL = `https://rhei-resume.pages.dev${url.pathname.replace(/^\/resume/, "")}${url.search}`;
+    return fetch(proxyURL, {
+      method: request.method,
+      headers: request.headers,
+      body: request.body,
+    });
+  }
+
+  // assets 프록시
+  const assetURL = `https://rhei-resume.pages.dev${url.pathname}${url.search}`;
+  return fetch(assetURL, {
     method: request.method,
     headers: request.headers,
     body: request.body,
