f14f2659acfb11a9abfa3ae132c5122eb7909cc7
Dahye <psst5491@naver.com>
2025-04-09 14:56:53 +0900

    💄(resume) add education

---
+++ apps/resume/app/routes/resume/_components/Education/index.tsx
@@ -0,0 +1,58 @@
+import { StickyHeading } from "@rhei/react";
+
+const EDUCATION = [
+  {
+    title: "서강대학교",
+    major: "철학과 / 컴퓨터공학과",
+    period: "2019.03 - 2023.08",
+    location: "서울시 마포구",
+    description: [
+      { content: "CGPA 3.94 / 4.5" },
+      { content: "전공 GPA 3.73 / 4.5" },
+    ],
+  },
+  {
+    title: "김해외국어고등학교",
+    major: "영어일본어과",
+    period: "2016.03 - 2019.02",
+    location: "경상남도",
+  },
+];
+
+export default function Education() {
+  return (
+    <div>
+      <StickyHeading>
+        <h2 className="text-h2 bg-normal">교육</h2>
+      </StickyHeading>
+
+      <ol className="flex list-disc flex-col gap-2 ps-4">
+        {EDUCATION.map((education) => (
+          <li key={education.title}>
+            <article>
+              <div className="flex flex-col justify-between gap-x-4 sm:flex-row">
+                <p>
+                  <span className="mr-2 font-extrabold">{education.title}</span>
+                  {education.major}
+                </p>
+
+                <p className="text-sub">
+                  <span className="mr-2">{education.period}</span>
+                  {education.location}
+                </p>
+              </div>
+
+              {education.description && (
+                <ul className="list-disc ps-4">
+                  {education.description?.map((desc) => (
+                    <li key={desc.content}>{desc.content}</li>
+                  ))}
+                </ul>
+              )}
+            </article>
+          </li>
+        ))}
+      </ol>
+    </div>
+  );
+}
--- apps/resume/app/routes/resume/index.tsx
+++ apps/resume/app/routes/resume/index.tsx
@@ -1,4 +1,7 @@
+import { StickyHeading } from "@rhei/react";
 import { GNB } from "@rhei/ui";
+
+import Education from "./_components/Education";
 
 export default function ResumePage() {
   return (
@@ -7,8 +10,16 @@ export default function ResumePage() {
         <GNB />
       </header>
 
-      <main className="content-x">
-        <section className="mx-auto max-w-6xl">content</section>
+      <main className="content-x text-p">
+        <section className="mx-auto max-w-6xl">
+          <h1>FRONT-END DEVELOPER</h1>
+
+          <StickyHeading>
+            <h2 className="text-h2 bg-normal">요약</h2>
+          </StickyHeading>
+
+          <Education />
+        </section>
       </main>
     </div>
   );
