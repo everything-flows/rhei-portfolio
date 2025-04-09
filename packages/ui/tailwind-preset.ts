d0f7ccf87142615a228e7e7eb7be5c8bbd9eb586
Dahye <psst5491@naver.com>
2025-04-09 12:40:23 +0900

    🔧 make resume dir & set tailwind

---
+++ packages/ui/tailwind-preset.ts
@@ -0,0 +1,32 @@
+import type { Config } from "tailwindcss";
+
+const preset: Partial<Config> = {
+  theme: {
+    extend: {
+      colors: {
+        gray: {
+          white: "#f9f9f9",
+          100: "#e5e5e5",
+          200: "#d4d4d4",
+          300: "#a3a3a3",
+          400: "#737373",
+          500: "#525252",
+          600: "#404040",
+          700: "#262626",
+          800: "#171717",
+          900: "#0a0a0a",
+          black: "#1a1a1a",
+        },
+        blue: {
+          500: "#3b82f6",
+        },
+        orange: {
+          500: "#f97316",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default preset;
--- packages/ui/tailwind.config.js
+++ packages/ui/tailwind.config.js
@@ -2,29 +2,7 @@
 module.exports = {
   content: ["./**/*.{ts,tsx}", "../../apps/**/*.{ts,tsx}"],
   theme: {
-    extend: {
-      colors: {
-        gray: {
-          white: "#ffffff",
-          100: "#f3f4f6",
-          200: "#e5e7eb",
-          300: "#d4d4d8",
-          400: "#a1a1aa",
-          500: "#71717a",
-          600: "#52525b",
-          700: "#3f3f46",
-          800: "#27272a",
-          900: "#18181b",
-          black: "#0a0a0a",
-        },
-        blue: {
-          500: "#0055ff",
-        },
-        orange: {
-          500: "#ff6000",
-        },
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
