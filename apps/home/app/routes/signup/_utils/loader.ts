969b0fdcaf5957268a7218501d8c02a60f08591d
Dahye <psst5491@naver.com>
2025-04-10 16:28:39 +0900

    🐛 fix loader args

--- apps/home/app/routes/signup/_utils/loader.ts
+++ apps/home/app/routes/signup/_utils/loader.ts
@@ -1,12 +1,17 @@
-export default async function loader() {
-  if (!process.env.SUPABASE_URL || !process.env.SUPABASE_ANON_KEY) {
+import { LoaderFunctionArgs } from "@remix-run/cloudflare";
+
+export default async function loader({ context }: LoaderFunctionArgs) {
+  const SUPABASE_URL = context.cloudflare.env.SUPABASE_URL;
+  const SUPABASE_ANON_KEY = context.cloudflare.env.SUPABASE_ANON_KEY;
+
+  if (!SUPABASE_URL || !SUPABASE_ANON_KEY) {
     throw new Error("Missing SUPABASE env variables");
   }
 
   return {
     env: {
-      SUPABASE_URL: process.env.SUPABASE_URL!,
-      SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY!,
+      SUPABASE_URL,
+      SUPABASE_ANON_KEY,
     },
   };
 }
