cc8fdf006be952e42bacfd08cdd1b5f0284def7c
Dahye <psst5491@naver.com>
2025-04-10 15:59:05 +0900

    🐛 error handling

--- apps/home/app/lib/supabase.ts
+++
@@ -1,7 +0,0 @@
-import { createBrowserClient } from "@supabase/ssr";
-import { env } from "~/env.server";
-
-export const supabase = createBrowserClient(
-  process.env.SUPABASE_URL || "",
-  process.env.SUPABASE_ANON_KEY || "",
-);
--- apps/home/app/routes/signin/_utils/loader.ts
+++ apps/home/app/routes/signin/_utils/loader.ts
@@ -1,4 +1,8 @@
 export default async function loader() {
+  if (!process.env.SUPABASE_URL || !process.env.SUPABASE_ANON_KEY) {
+    throw new Error("Missing SUPABASE env variables");
+  }
+
   return {
     env: {
       SUPABASE_URL: process.env.SUPABASE_URL!,
