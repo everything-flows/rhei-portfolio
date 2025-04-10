cd1dae9aeb066db9043f0c51794d21f7268706a8
Dahye <psst5491@naver.com>
2025-04-10 16:02:01 +0900

    🐛 error handling

--- apps/home/app/routes/_index/_utils/loader.ts
+++ apps/home/app/routes/_index/_utils/loader.ts
@@ -8,25 +8,28 @@ import {
 export default async function loader({ request }: LoaderFunctionArgs) {
   const headers = new Headers();
 
-  const supabase = createServerClient(
-    process.env.SUPABASE_URL!,
-    process.env.SUPABASE_ANON_KEY!,
-    {
-      cookies: {
-        getAll() {
-          return parseCookieHeader(request.headers.get("Cookie") ?? "");
-        },
-        setAll(cookiesToSet) {
-          cookiesToSet.forEach(({ name, value, options }) =>
-            headers.append(
-              "Set-Cookie",
-              serializeCookieHeader(name, value, options),
-            ),
-          );
-        },
+  const SUPABASE_URL = process.env.SUPABASE_URL;
+  const SUPABASE_ANON_KEY = process.env.SUPABASE_ANON_KEY;
+
+  if (!process.env.SUPABASE_URL || !process.env.SUPABASE_ANON_KEY) {
+    throw new Error("Missing SUPABASE env variables");
+  }
+
+  const supabase = createServerClient(SUPABASE_URL!, SUPABASE_ANON_KEY!, {
+    cookies: {
+      getAll() {
+        return parseCookieHeader(request.headers.get("Cookie") ?? "");
+      },
+      setAll(cookiesToSet) {
+        cookiesToSet.forEach(({ name, value, options }) =>
+          headers.append(
+            "Set-Cookie",
+            serializeCookieHeader(name, value, options),
+          ),
+        );
       },
     },
-  );
+  });
 
   const {
     data: { session },
