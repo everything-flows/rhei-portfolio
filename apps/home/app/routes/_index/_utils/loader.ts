5a044de5bd99e317661fb68d0730d1e3139f7784
Dahye <psst5491@naver.com>
2025-04-10 20:06:30 +0900

    🐛 exception handling

--- apps/home/app/routes/_index/_utils/loader.ts
+++ apps/home/app/routes/_index/_utils/loader.ts
@@ -12,7 +12,7 @@ export default async function loader({ context, request }: LoaderFunctionArgs) {
   const SUPABASE_ANON_KEY = context.cloudflare.env.SUPABASE_ANON_KEY;
 
   if (!SUPABASE_URL || !SUPABASE_ANON_KEY) {
-    throw new Error("Missing SUPABASE env variables");
+    return { users: null };
   }
 
   const supabase = createServerClient(SUPABASE_URL!, SUPABASE_ANON_KEY!, {
