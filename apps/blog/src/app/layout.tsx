cd6b50136877e77d0cb4cf18cf11aa11c1ba7a20
Dahye <psst5491@naver.com>
2025-04-11 17:33:10 +0900

    💄(blog) basic template

--- apps/blog/src/app/layout.tsx
+++ apps/blog/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import GNBWrapper from "./_components/GNBWrapper";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -13,7 +14,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body>{children}</body>
+      <body className="bg-normal text-normal">
+        <GNBWrapper />
+        {children}
+      </body>
     </html>
   );
 }
--- apps/blog/src/app/page.tsx
+++ apps/blog/src/app/page.tsx
@@ -1,17 +1,11 @@
-import GNBWrapper from "./_components/GNBWrapper";
-
 export default async function Home() {
   return (
-    <>
-      <GNBWrapper />
-
-      <main className="content-x">
-        <section className="mx-auto max-w-6xl">section</section>
-        <section className="mx-auto max-w-6xl">section</section>
-        <section className="mx-auto max-w-6xl">section</section>
-        <section className="mx-auto max-w-6xl">section</section>
-      </main>
-    </>
+    <main className="content-x">
+      <section className="mx-auto max-w-6xl">section</section>
+      <section className="mx-auto max-w-6xl">section</section>
+      <section className="mx-auto max-w-6xl">section</section>
+      <section className="mx-auto max-w-6xl">section</section>
+    </main>
   );
 }
 
