860da20e225665e098eb8a4daad0b24b50aa7dfb
Dahye <psst5491@naver.com>
2025-04-11 19:01:52 +0900

    ➕ add prettierrc

--- apps/blog/src/app/_components/PinnedSection/PinnedHeader.tsx
+++ apps/blog/src/app/_components/PinnedSection/PinnedHeader.tsx
@@ -1,10 +1,10 @@
 export default function PinnedHeader() {
   return (
-    <h2 className="text-responsive-h2 items-center flex flex-wrap gap-x-2 gap-y-1 leading-[1.4] font-black justify-end">
+    <h2 className="text-responsive-h2 flex flex-wrap items-center justify-end gap-x-2 gap-y-1 leading-[1.4] font-black">
       <span className="text-block">📌</span>
-      <hr className="flex-1 border border-nromal" />
+      <hr className="border-normal flex-1 border" />
       <span className="text-block bg-reverse text-reverse">Pinned</span>
-      <span className="text-block border-2 border-reverse">Posts</span>
+      <span className="text-block border-reverse border-2">Posts</span>
     </h2>
   );
 }
--- apps/blog/src/app/_components/PinnedSection/index.tsx
+++ apps/blog/src/app/_components/PinnedSection/index.tsx
@@ -5,12 +5,12 @@ export default async function PinnedSection() {
   const data = await getPinnedPostList();
 
   return (
-    <section className="mx-auto max-w-6xl flex gap-4 flex-col">
+    <section className="mx-auto flex max-w-6xl flex-col gap-4">
       <PinnedHeader />
 
-      <ul className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+      <ul className="grid grid-cols-1 gap-4 sm:grid-cols-2">
         {data.map((post) => (
-          <li key={post.id} className="p-4 border rounded-lg">
+          <li key={post.id} className="rounded-lg border p-4">
             <a href={`/blog/${post.subBlog}/${post.id}`}>
               <h3 className="text-xl font-semibold">{post.title}</h3>
               <p>{post.subTitle}</p>
