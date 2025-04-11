5e8d98176d195499cdcea46ab0c2546c70904a6e
Dahye <psst5491@naver.com>
2025-04-11 21:28:25 +0900

    💄 add logo-styled title

--- apps/home/app/routes/_index/_components/About/index.tsx
+++ apps/home/app/routes/_index/_components/About/index.tsx
@@ -8,7 +8,15 @@ export default function About() {
       </StickyHeading>
 
       <StickyHeading>
-        <h3 className="bg-normal text-[2rem]">안녕하세요!</h3>
+        <h3 className="text-brand w-full text-center text-[clamp(2rem,6vw,3rem)] font-bold">
+          <span className="bg-brand inline-block skew-x-[-50deg] rounded-[100%] px-2 py-2">
+            <span className="bg-normal inline-block skew-y-[2deg] rounded-[100%] px-2 py-2">
+              <span className="font-PyeongChangPeaceBold inline-block skew-x-[51deg] skew-y-[-2deg]">
+                Panta Rhei
+              </span>
+            </span>
+          </span>
+        </h3>
       </StickyHeading>
 
       <p>
--- apps/home/app/routes/_index/_components/Banner/index.tsx
+++ apps/home/app/routes/_index/_components/Banner/index.tsx
@@ -4,13 +4,13 @@ export default function Banner() {
   return (
     <section>
       <Marquee style="-content-x bg-blue-500 dark:bg-orange-500 overflow-x-clip">
-        <p className="whitespace-nowrap px-4 py-2 text-[clamp(1rem,4vw,2rem)] font-bold text-gray-white">
-          보고 싶은 것도 보여주고 싶은 것도 많은
+        <p className="text-reverse whitespace-nowrap px-4 py-2 text-[clamp(1rem,4vw,2rem)] font-extrabold">
+          배너 텍스트 1
         </p>
       </Marquee>
       <Marquee style="-content-x overflow-x-clip" reverse>
-        <p className="whitespace-nowrap px-4 py-2 text-[clamp(1rem,4vw,2rem)] font-bold text-blue-500 dark:text-orange-500">
-          보고 싶은 것도 보여주고 싶은 것도 많은
+        <p className="whitespace-nowrap px-4 py-2 text-[clamp(1rem,4vw,2rem)] font-extrabold text-blue-500 dark:text-orange-500">
+          배너 텍스트 2
         </p>
       </Marquee>
     </section>
