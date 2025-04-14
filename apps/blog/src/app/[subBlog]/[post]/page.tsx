b4e39b99fd5e1b5cbdb7077e307018574f3ac5e3
Dahye <psst5491@naver.com>
2025-04-14 15:54:30 +0900

    💄 post page template

---
+++ apps/blog/src/app/[subBlog]/[post]/page.tsx
@@ -0,0 +1,8 @@
+export default function PostPage() {
+  return (
+    <main className="content-x">
+      <section>header</section>
+      <section>body</section>
+    </main>
+  );
+}
---
+++ apps/blog/src/app/[subBlog]/page.tsx
@@ -0,0 +1,3 @@
+export default function SubBlogPage() {
+  return <div className="content-x">sub blog</div>;
+}
