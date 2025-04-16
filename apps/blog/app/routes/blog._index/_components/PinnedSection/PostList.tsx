307b542f4095070a77d5286e16e981a0adc9c2ad
Dahye <psst5491@naver.com>
2025-04-16 16:03:09 +0900

    ✨(blog) first post link

--- apps/blog/app/routes/blog._index/_components/PinnedSection/PostList.tsx
+++ apps/blog/app/routes/blog._index/_components/PinnedSection/PostList.tsx
@@ -1,4 +1,7 @@
-export default function PostList({ postList }) {
+import { Link } from "@remix-run/react";
+import { Document } from "~/types/post";
+
+export default function PostList({ postList }: { postList: Document[] }) {
   if (!postList || postList.length === 0) {
     return null;
   }
@@ -8,33 +11,35 @@ export default function PostList({ postList }) {
 
   return (
     <>
-      <article className="mt-6 grid grid-cols-1 gap-4 sm:grid-cols-[1fr_1fr] md:grid-cols-[2fr_3fr] lg:grid-cols-[1fr_2fr]">
-        <img
-          src={firstPost.thumbnail}
-          alt={`${firstPost.title}`}
-          className="aspect-[16/9] w-full shrink-0 rounded-3xl border border-gray-200 object-cover sm:aspect-[4/3] dark:border-gray-600"
-        />
-        <div>
-          <h3 className="text-responsive-h2 font-900">{firstPost.title}</h3>
-          <p className="text-responsive-p">{firstPost.subTitle}</p>
-          <div className="flex flex-wrap gap-x-2 gap-y-1">
-            {firstPost.tags.map((tag) => (
-              <p
-                key={tag.title}
-                className="text-responsive-p text-brand border-brand mt-2 rounded-full border-[1.5px] px-2"
-              >
-                {tag.title}
-              </p>
-            ))}
+      <Link to={`/blog/${firstPost.subBlog}/${firstPost.id}`}>
+        <article className="mt-6 grid grid-cols-1 gap-4 sm:grid-cols-[1fr_1fr] md:grid-cols-[2fr_3fr] lg:grid-cols-[1fr_2fr]">
+          <img
+            src={firstPost.thumbnail}
+            alt={`${firstPost.title}`}
+            className="aspect-[16/9] w-full shrink-0 rounded-3xl border border-gray-200 object-cover sm:aspect-[4/3] dark:border-gray-600"
+          />
+          <div>
+            <h3 className="text-responsive-h2 font-900">{firstPost.title}</h3>
+            <p className="text-responsive-p">{firstPost.subTitle}</p>
+            <div className="flex flex-wrap gap-x-2 gap-y-1">
+              {firstPost.tags.map((tag) => (
+                <p
+                  key={tag.title}
+                  className="text-responsive-p text-brand border-brand mt-2 rounded-full border-[1.5px] px-2"
+                >
+                  {tag.title}
+                </p>
+              ))}
+            </div>
           </div>
-        </div>
-      </article>
+        </article>
+      </Link>
 
       {leftPostList.length > 0 && (
         <ul className="mt-4 flex gap-4 overflow-auto">
-          {leftPostList.map((post) => (
+          {leftPostList.map((post: Document) => (
             <li key={post.id}>
-              <a href={`/blog/${post.subBlog}/${post.id}`}>
+              <Link to={`/blog/${post.subBlog}/${post.id}`}>
                 <article className="grid w-[min(80dvw,280px)] grid-rows-[auto,auto] gap-3">
                   {post.thumbnail ? (
                     <img
@@ -54,7 +59,7 @@ export default function PostList({ postList }) {
                     <p className="text-p">{post.subTitle}</p>
                   </div>
                 </article>
-              </a>
+              </Link>
             </li>
           ))}
         </ul>
