ea02fced916866220c06fafde356ff7632aa03b4
Dahye <psst5491@naver.com>
2025-04-16 19:48:09 +0900

    🧪 fix blog proxy

--- apps/home/functions/blog/[[path]].ts
+++ apps/home/functions/blog/[[path]].ts
@@ -2,15 +2,13 @@ export const onRequest: PagesFunction = async ({ request }) => {
   const url = new URL(request.url);
 
   const isBlogRequest = url.pathname.startsWith("/blog");
-  const isDataRequest = url.pathname.endsWith(".data");
 
   if (!isBlogRequest) {
     return new Response("Not found", { status: 404 });
   }
 
-  if (isDataRequest) {
-    const actualPath = url.pathname.replace(/\.data$/, "/.data");
-    return fetch(`https://rhei-blog.pages.dev${actualPath}${url.search}`, {
+  if (url.pathname === "/blog.data") {
+    return fetch("https://rhei-blog.pages.dev/blog/.data", {
       method: request.method,
       headers: request.headers,
       body:
