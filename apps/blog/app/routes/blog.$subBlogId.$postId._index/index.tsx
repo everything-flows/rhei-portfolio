0f3b1a790a49eeb8f1c60a49b313984e2b5a670e
Dahye <psst5491@naver.com>
2025-04-16 15:40:41 +0900

    🚚  make post page route

---
+++ apps/blog/app/routes/blog.$subBlogId.$postId._index/index.tsx
@@ -0,0 +1,13 @@
+import { GNB } from "@rhei/ui";
+
+export default function PostPage() {
+  return (
+    <>
+      <header className="content-x">
+        <GNB />
+      </header>
+
+      <main className="content-x">post here</main>
+    </>
+  );
+}
