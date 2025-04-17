5c2b8e4a5ed974f37526f30c742015c9cfe010fb
Dahye <psst5491@naver.com>
2025-04-17 15:01:59 +0900

    🧪 fix asset route

--- apps/blog/vite.config.ts
+++ apps/blog/vite.config.ts
@@ -6,10 +6,13 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 declare module "@remix-run/cloudflare" {
-  interface Future {}
+  interface Future {
+    v3_singleFetch: true;
+  }
 }
 
 export default defineConfig({
+  build: { assetsDir: "blog/assets" },
   plugins: [
     remixCloudflareDevProxy(),
     remix({
