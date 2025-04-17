9510a0cc9eab2b02d9f40586091163528a380b2b
Dahye <psst5491@naver.com>
2025-04-17 14:33:06 +0900

    🧪 fix proxy function

--- apps/home/functions/blog/[[path]].ts
+++ apps/home/functions/blog/[[path]].ts
@@ -1,8 +1,6 @@
 export const onRequest: PagesFunction = async ({ request }) => {
   const url = new URL(request.url);
-
-  const pathWithoutBlog = url.pathname.replace(/^\/blog/, "") || "/";
-  const targetUrl = `https://rhei-blog.pages.dev${pathWithoutBlog}${url.search}`;
+  const targetUrl = `https://rhei-blog.pages.dev${url.pathname.replace(/^\/blog/, "") || "/"}${url.search}`;
 
   const safeHeaders = new Headers();
   safeHeaders.set("Accept", request.headers.get("Accept") || "*/*");
