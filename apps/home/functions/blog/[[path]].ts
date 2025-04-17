da53104547a8a7a1e83d2345b055c53e38dd2517
Dahye <psst5491@naver.com>
2025-04-17 11:07:39 +0900

    🧪 remove .data related code

--- apps/home/functions/blog/[[path]].ts
+++ apps/home/functions/blog/[[path]].ts
@@ -2,17 +2,9 @@ export const onRequest: PagesFunction = async ({ request }) => {
   const url = new URL(request.url);
 
   const isBlogRequest = url.pathname.startsWith("/blog");
-  const isDataRequest = url.pathname === "/blog.data";
 
   if (!isBlogRequest) {
     return new Response("Not found", { status: 404 });
-  }
-
-  if (isDataRequest) {
-    return fetch("https://rhei-blog.pages.dev/blog/.data", {
-      method: request.method,
-      headers: request.headers,
-    });
   }
 
   return fetch(`https://rhei-blog.pages.dev${url.pathname}${url.search}`, {
