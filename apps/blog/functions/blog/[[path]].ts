d0331346d5d7c38f48667c9f13bebd37ba8f5dec
Dahye <psst5491@naver.com>
2025-04-17 13:00:04 +0900

    🧪(blog) fix proxy function

--- apps/blog/functions/blog/[[path]].ts
+++ apps/blog/functions/blog/[[path]].ts
@@ -1,32 +1,20 @@
 export const onRequest: PagesFunction = async ({ request }) => {
   const url = new URL(request.url);
-  const { pathname, search } = url;
+  let targetUrl = "";
 
-  const match = pathname.match(/^(.+)\.data$/);
-
-  if (match && !pathname.endsWith("/.data")) {
-    const targetPath = `${match[1]}/.data`;
-    const rewrittenUrl = new URL(
-      targetPath + search,
-      "https://rhei-blog.pages.dev",
-    );
-
-    console.log("Rewriting:", pathname, "→", rewrittenUrl.pathname);
-
-    return fetch(rewrittenUrl.toString(), {
-      method: request.method,
-      headers: request.headers,
-      body:
-        request.method !== "GET" && request.method !== "HEAD"
-          ? request.body
-          : undefined,
-    });
+  if (url.pathname.endsWith(".data") && !url.pathname.endsWith("/.data")) {
+    const pathname = url.pathname.split(".data");
+    targetUrl = `https://rhei-blog.pages.dev${pathname}/.data`;
+  } else {
+    targetUrl = `https://rhei-blog.pages.dev${url.pathname}${url.search}`;
   }
 
-  const passthroughUrl = new URL(
-    pathname + search,
-    "https://rhei-blog.pages.dev",
-  );
-
-  return fetch(passthroughUrl.toString(), request);
+  return fetch(targetUrl, {
+    method: request.method,
+    headers: request.headers,
+    body:
+      request.method !== "GET" && request.method !== "HEAD"
+        ? request.body
+        : null,
+  });
 };
