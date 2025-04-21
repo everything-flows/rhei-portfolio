dd861b87c64a5e64d3a2f3a67d6b17c861e37a1c
Dahye <psst5491@naver.com>
2025-04-21 23:31:39 +0900

    🧪(blog) fix config

--- apps/blog/vite.config.ts
+++ apps/blog/vite.config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
   plugins: [
     remixCloudflareDevProxy(),
     remix({
-      basename: "/blog",
+      basename: "/blog/",
       future: {
         v3_fetcherPersist: true,
         v3_relativeSplatPath: true,
