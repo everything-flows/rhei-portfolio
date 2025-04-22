9e1fd6c70bc7a7f3c2380759bfa5c73c5b0be53b
Dahye <psst5491@naver.com>
2025-04-22 15:36:16 +0900

    💄 make pill frame

--- apps/craft/src/app/spinningPill/Pill.tsx
+++ apps/craft/src/app/spinningPill/Pill.tsx
@@ -1,3 +1,42 @@
 export default function Pill() {
-  return <div>pill here</div>;
+  return (
+    <div className="relative m-40 size-40">
+      <div
+        className="border-brand absolute rounded-full border-2"
+        style={{ top: "-4rem", width: "4rem", height: "4rem" }}
+      />
+      <div
+        className="border-brand absolute border-2"
+        style={{ top: "-2rem", width: "4rem", height: "4rem" }}
+      />
+      <div
+        className="border-brand absolute rounded-full border-2"
+        style={{
+          top: 0,
+          width: "4rem",
+          height: "4rem",
+          transform: "scaleY(0)",
+        }}
+      />
+
+      <div
+        className="border-reverse absolute rounded-full border-2"
+        style={{
+          top: 0,
+          width: "4rem",
+          height: "4rem",
+          transform: "scaleY(0)",
+        }}
+      />
+      <div
+        className="border-reverse absolute border-2"
+        style={{ top: "2rem", width: "4rem", height: "4rem" }}
+      />
+      <div
+        className="border-reverse absolute rounded-full border-2"
+        style={{ top: "4rem", width: "4rem", height: "4rem" }}
+      />
+      <div></div>
+    </div>
+  );
 }
