83ad02439650c5f171262552a926431120fe86bc
Dahye <psst5491@naver.com>
2025-04-22 18:56:30 +0900

    🎨(pill) use constants

--- apps/craft/src/app/spinningPill/Pill.tsx
+++ apps/craft/src/app/spinningPill/Pill.tsx
@@ -2,13 +2,23 @@
 
 import { useEffect, useRef, useState } from "react";
 
-export default function Pill() {
+export default function Pill({
+  width = "4rem",
+  height = "3rem",
+  speed = 500,
+  fill = true,
+}: {
+  width?: string;
+  height?: string;
+  speed?: number;
+  fill?: boolean;
+}) {
   const startTimeRef = useRef<number | null>(null);
   const [sin, setSin] = useState(0);
   const [cos, setCos] = useState(0);
   const [absSin, setAbsSin] = useState(0);
-  const [blueTop, setBlueTop] = useState("calc(2rem)");
-  const [blackTop, setBlackTop] = useState("calc(2rem)");
+  const [blueTop, setBlueTop] = useState(`calc(${width} / 2)`);
+  const [blackTop, setBlackTop] = useState(`calc(${width} / 2)`);
 
   useEffect(() => {
     function rotate(timestamp: number) {
@@ -16,7 +26,7 @@ export default function Pill() {
         return;
       }
 
-      const value = (timestamp - startTimeRef.current) / 500;
+      const value = (timestamp - startTimeRef.current) / speed;
 
       const s = Math.sin(value);
 
@@ -25,12 +35,12 @@ export default function Pill() {
       setAbsSin(Math.abs(s));
       if (s < 0) {
         // blue is top, black is bottom
-        setBlueTop(`calc(${s} * 3rem + 2rem)`);
-        setBlackTop("2rem");
+        setBlueTop(`calc(${s} * ${height} + ${width} / 2)`);
+        setBlackTop(`calc(${width} / 2)`);
       } else {
         // blue is bottom, black is top
-        setBlueTop("2rem");
-        setBlackTop(`calc(${s} * -3rem + 2rem)`);
+        setBlueTop(`calc(${width} / 2)`);
+        setBlackTop(`calc(${s} * -${height} + ${width} / 2)`);
       }
       requestAnimationFrame((t) => rotate(t));
     }
@@ -41,67 +51,70 @@ export default function Pill() {
     }
 
     requestAnimationFrame(firstFrame);
-  }, []);
+  }, [width, height]);
 
   return (
-    <div className="relative m-4 h-20 w-8">
+    <div
+      className="relative"
+      style={{ width, height: `calc(${height} * 2 + ${width})` }}
+    >
       {/* circle */}
       <div
-        className="border-brand bg-brand absolute rounded-full border-2"
+        className={`border-brand ${fill ? "bg-brand" : ""} absolute rounded-full border-2`}
         style={{
-          top: `calc(${sin} * 3rem)`,
-          width: "4rem",
-          height: "4rem",
+          top: `calc(${sin} * ${height} + ${height})`,
+          width: width,
+          height: width,
           zIndex: cos > 0 ? 0 : 1,
         }}
       />
       <div
-        className="border-reverse bg-reverse absolute rounded-full border-2"
+        className={`border-reverse ${fill ? "bg-reverse" : ""} absolute rounded-full border-2`}
         style={{
-          top: `calc(-1 * ${sin} * 3rem)`,
-          width: "4rem",
-          height: "4rem",
+          top: `calc(-1 * ${sin} * ${height} + ${height})`,
+          width: width,
+          height: width,
           zIndex: cos > 0 ? 1 : 0,
         }}
       />
 
       {/* square */}
       <div
-        className="border-brand bg-brand absolute border-2"
+        className={`border-brand ${fill ? "bg-brand" : ""} absolute border-2`}
         style={{
-          top: blueTop,
-          width: "4rem",
-          height: `calc(${absSin} * 3rem)`,
+          top: `calc(${blueTop} + ${height})`,
+          width: width,
+          height: `calc(${absSin} * ${height})`,
           zIndex: cos > 0 ? 0 : 1,
         }}
       />
       <div
-        className="border-reverse bg-reverse absolute border-2"
+        className={`border-reverse ${fill ? "bg-reverse" : ""} absolute border-2`}
         style={{
-          top: blackTop,
-          width: "4rem",
-          height: `calc(${absSin} * 3rem)`,
+          top: `calc(${blackTop} + ${height})`,
+          width: width,
+          height: `calc(${absSin} * ${height})`,
           zIndex: cos > 0 ? 1 : 0,
         }}
       />
 
       {/* center circle */}
       <div
-        className="border-brand bg-brand absolute rounded-full border-2"
+        className={`border-brand ${fill ? "bg-brand" : ""} absolute rounded-full border-2`}
         style={{
-          top: 0,
-          width: "4rem",
-          height: "4rem",
+          top: height,
+          width: width,
+          height: width,
           transform: `scaleY(${1 - absSin})`,
           zIndex: cos > 0 ? 0 : 1,
         }}
       />
       <div
-        className="border-reverse bg-reverse absolute rounded-full border-2"
+        className={`border-reverse ${fill ? "bg-reverse" : ""} absolute rounded-full border-2`}
         style={{
-          top: 0,
-          width: "4rem",
-          height: "4rem",
+          top: height,
+          width: width,
+          height: width,
           transform: `scaleY(${1 - absSin})`,
           zIndex: cos > 0 ? 1 : 0,
         }}
--- apps/craft/src/app/spinningPill/page.tsx
+++ apps/craft/src/app/spinningPill/page.tsx
@@ -5,10 +5,47 @@ export default async function SpinningPillPage() {
   return (
     <>
       <main className="content-x">
-        <h1>Fake 3D 알약 만들기</h1>
+        <article className="mx-auto flex w-full max-w-6xl flex-col gap-8">
+          <h1 className="text-[2rem] font-bold">Fake 3D 알약</h1>
 
-        <article className="mx-auto w-full max-w-6xl">
-          <Pill />
+          <section className="flex flex-col gap-4">
+            <h2>기본</h2>
+
+            <div className="flex flex-wrap justify-center gap-8">
+              <Pill />
+              <Pill fill={false} />
+            </div>
+          </section>
+
+          <section className="flex flex-col justify-center gap-4">
+            <h2>크기 변화</h2>
+
+            <div className="flex flex-wrap justify-center gap-8">
+              <div className="flex flex-wrap justify-center gap-8">
+                <Pill width="3rem" height="1.5rem" />
+                <Pill width="3rem" height="1.5rem" fill={false} />
+              </div>
+              <div className="flex flex-wrap justify-center gap-8">
+                <Pill width="5rem" height="2.5rem" />
+                <Pill width="5rem" height="2.5rem" fill={false} />
+              </div>
+            </div>
+          </section>
+
+          <section className="flex flex-col justify-center gap-4">
+            <h2>속도 변화</h2>
+
+            <div className="flex flex-wrap justify-center gap-8">
+              <div className="flex flex-wrap justify-center gap-8">
+                <Pill speed={300} />
+                <Pill speed={300} fill={false} />
+              </div>
+              <div className="flex flex-wrap justify-center gap-8">
+                <Pill speed={700} />
+                <Pill speed={700} fill={false} />
+              </div>
+            </div>
+          </section>
         </article>
       </main>
 
