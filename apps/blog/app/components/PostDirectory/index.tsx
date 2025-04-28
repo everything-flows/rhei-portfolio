31630116ebc654b63b697c1e6002af513ef3676b
Dahye <psst5491@naver.com>
2025-04-28 15:11:37 +0900

    💄 fix post directory

--- apps/blog/app/components/PostDirectory/index.tsx
+++ apps/blog/app/components/PostDirectory/index.tsx
@@ -8,20 +8,25 @@ export default function PostDirectory({ postList }: { postList: Document[] }) {
         {postList?.map((post) => (
           <li key={post.id}>
             <Link to={`/${post.subBlog}/${post.id}`}>
-              <article className="grid grid-cols-1 gap-4 py-1 sm:grid-cols-[10rem_auto]">
+              <article className="grid grid-cols-[4rem_auto] gap-8 py-2 sm:grid-cols-[10rem_auto]">
                 {post.thumbnail ? (
                   <img
                     src={post.thumbnail}
-                    className="aspect-[4/3] w-full shrink-0 rounded-2xl border border-gray-200 object-cover dark:border-gray-600"
+                    className="aspect-[1/1] w-full shrink-0 rounded-full border border-gray-200 object-cover sm:aspect-[4/3] sm:rounded-2xl dark:border-gray-600"
                   />
                 ) : (
-                  <div className="aspect-[4/3] w-full shrink-0 overflow-hidden rounded-2xl border border-gray-200 object-cover dark:border-gray-600">
-                    <div className="bg-thumbnail h-full w-full rounded-3xl opacity-[0.8] blur-[80px]" />
+                  <div className="aspect-[1/1] w-full shrink-0 overflow-hidden rounded-full border border-gray-200 object-cover sm:aspect-[4/3] sm:rounded-2xl dark:border-gray-600">
+                    <div className="bg-thumbnail h-full w-full opacity-[0.8] blur-[80px]" />
                   </div>
                 )}
-                <div className="flex flex-col py-2">
-                  <h2 className="text-responsive-h3">{post.title}</h2>
-                  <p className="text-responsive-hp">{post.subTitle}</p>
+                <div className="flex gap-2">
+                  <h2 className="text-responsive-h3 sm:py-2">{post.emoji}</h2>
+                  <div className="flex flex-col sm:py-2">
+                    <h2 className="text-responsive-h3">{post.title}</h2>
+                    <p className="text-responsive-p font-normal text-gray-300">
+                      {post.subTitle}
+                    </p>
+                  </div>
                 </div>
               </article>
             </Link>
--- apps/blog/app/routes/$subBlogId.$postId._index/_components/PostHeader.tsx
+++ apps/blog/app/routes/$subBlogId.$postId._index/_components/PostHeader.tsx
@@ -5,7 +5,7 @@ export default function PostHeader({ data }: { data: { postData: Document } }) {
   const { title, subTitle } = postData;
 
   return (
-    <section className="mx-auto max-w-6xl">
+    <section className="mx-auto mb-8 max-w-6xl border-b border-gray-200 pb-8 dark:border-gray-600">
       <h1 className="text-responsive-h1 break-keep">{title}</h1>
       <h2 className="text-responsive-p break-keep">{subTitle}</h2>
     </section>
