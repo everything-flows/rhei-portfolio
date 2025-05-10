4ce9e28464f9126123c203c8811f82ef176f5966
Dahye <psst5491@naver.com>
2025-05-10 14:56:11 +0900

    ✨(blog) add all post page

--- apps/blog/app/routes/_index/_components/RecentSection/RecentHeader.tsx
+++ apps/blog/app/routes/_index/_components/RecentSection/RecentHeader.tsx
@@ -1,12 +1,16 @@
+import { Link } from "@remix-run/react";
+
 export default function RecentHeader() {
   return (
-    <h2 className="text-responsive-display flex flex-wrap items-center justify-end gap-x-2 gap-y-1 font-black leading-[1.4]">
-      <span className="text-block border-reverse border-2">View</span>
-      <span className="text-block bg-reverse text-reverse border-reverse border-2">
-        All
-      </span>
-      <hr className="border-reverse flex-1 border" />
-      <span className="text-block border-reverse border-2">→</span>
-    </h2>
+    <Link to="/all" className="hover:text-brand">
+      <h2 className="text-responsive-display flex flex-wrap items-center justify-end gap-x-2 gap-y-1 font-black leading-[1.4]">
+        <span className="text-block border-reverse border-2">View</span>
+        <span className="text-block bg-reverse text-reverse border-reverse border-2">
+          All
+        </span>
+        <hr className="border-reverse flex-1 border" />
+        <span className="text-block border-reverse border-2">→</span>
+      </h2>
+    </Link>
   );
 }
