b38078c3a84790baa54a16da67349d52a89302fc
Dahye <psst5491@naver.com>
2025-05-20 16:04:06 +0900

    💄(debounce) init event view

--- apps/debounceExample/main.ts
+++ apps/debounceExample/main.ts
@@ -1,5 +1,35 @@
 import "./style.css";
 
+const tickTime = 400; // ms
+const tickCount = 50;
+
 document.addEventListener("DOMContentLoaded", function () {
-  console.log("GM!");
+  initRawEventContainer();
+  initDebouncedEventContainer();
 });
+
+function initRawEventContainer() {
+  const rawEventContainer = document.getElementById("raw-event-container");
+  for (let i = 0; i < tickCount; i++) {
+    if (!rawEventContainer) {
+      return;
+    }
+    const element = document.createElement("div");
+    element.setAttribute("class", "tick-element");
+    rawEventContainer.appendChild(element);
+  }
+}
+
+function initDebouncedEventContainer() {
+  const debouncedEventContainer = document.getElementById(
+    "debounced-event-container"
+  );
+  for (let i = 0; i < tickCount; i++) {
+    if (!debouncedEventContainer) {
+      return;
+    }
+    const element = document.createElement("div");
+    element.setAttribute("class", "tick-element");
+    debouncedEventContainer.appendChild(element);
+  }
+}
