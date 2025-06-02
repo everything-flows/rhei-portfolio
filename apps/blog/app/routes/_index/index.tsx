907fdc86b57b68146e52c9080e331b9c5aa7d5aa
Dahye <psst5491@naver.com>
2025-06-02 21:33:37 +0900

    🔥(blog) remove alert section

--- apps/blog/app/routes/_index/index.tsx
+++ apps/blog/app/routes/_index/index.tsx
@@ -1,4 +1,4 @@
-import { Link, useLoaderData } from "@remix-run/react";
+import { useLoaderData } from "@remix-run/react";
 import { Footer, GNB } from "@rhei/ui";
 
 import PinnedSection from "./_components/PinnedSection";
@@ -22,20 +22,6 @@ export default function Index() {
       </header>
 
       <main className="content-x flex flex-col">
-        <section className="mx-auto mb-8 flex w-full max-w-6xl flex-col rounded-xl bg-blue-200/30 px-6 py-4 dark:bg-orange-300/20">
-          <p>
-            잠깐!
-            <br />
-            블로그 서비스를{" "}
-            <Link to="https://psst54.me" className="text-brand underline">
-              psst54.me
-            </Link>
-            에서 이전중이에요.
-            <br />
-            아직 작동하지 않는 기능이 있을 수 있어요.
-          </p>
-        </section>
-
         <PinnedSection postList={pinnedPostList} />
         <RecentSection postList={recentPostList} />
       </main>
