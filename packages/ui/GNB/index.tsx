4e1a613780b47c9c3d8a7ffec8aaab21117eb2ae
Dahye <psst5491@naver.com>
2025-06-04 15:10:01 +0900

    💄 add GNB item padding

--- packages/ui/GNB/index.tsx
+++ packages/ui/GNB/index.tsx
@@ -28,7 +28,7 @@ export default function GNB({
           <li key={item.title}>
             <a
               href={item.link}
-              className={`font-extrabold text-[1.2rem] hover:text-brand ${
+              className={`font-extrabold text-[1.2rem] hover:text-brand px-1 ${
                 item.isCurrentTab() ? "text-brand" : ""
               }`}
             >
