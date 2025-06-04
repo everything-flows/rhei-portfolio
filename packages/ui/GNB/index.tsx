bac20bd154e44b6ea2fc91f91f159791b8516ed6
Dahye <psst5491@naver.com>
2025-06-04 15:08:53 +0900

    💄 highlight current tab in GNB

--- packages/ui/GNB/index.tsx
+++ packages/ui/GNB/index.tsx
@@ -28,7 +28,9 @@ export default function GNB({
           <li key={item.title}>
             <a
               href={item.link}
-              className="font-extrabold text-[1.2rem] hover:text-brand"
+              className={`font-extrabold text-[1.2rem] hover:text-brand ${
+                item.isCurrentTab() ? "text-brand" : ""
+              }`}
             >
               {item.title}
             </a>
