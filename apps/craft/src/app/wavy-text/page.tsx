a08562352baee46c4b5be0e751dab630bc6b0749
Dahye <psst5491@naver.com>
2025-06-09 14:11:22 +0900

    💬(craft) add example text

--- apps/craft/src/app/wavy-text/page.tsx
+++ apps/craft/src/app/wavy-text/page.tsx
@@ -11,11 +11,27 @@ export default function Page() {
 
         <section className="mx-auto w-full max-w-6xl">
           <p>
+            This is <span className="text-blue-500">blue</span> and{" "}
+            <strong>strong</strong> and <WavyText>wavvvvvvvvvy</WavyText> text
+          </p>
+
+          <p>
             This is{" "}
             <WavyText>
               <span className="text-blue-500">blue</span> and{" "}
-              <strong>strong</strong> and wavvvvvvvvvy text
-            </WavyText>
+              <strong>strong</strong> and wavvvvvvvvvy
+            </WavyText>{" "}
+            text
+          </p>
+
+          <p>
+            This is{" "}
+            <WavyText>
+              <span className="text-blue-500">
+                <strong>blue and strong and wavvvvvvvvvy </strong>
+              </span>
+            </WavyText>{" "}
+            text
           </p>
         </section>
       </main>
