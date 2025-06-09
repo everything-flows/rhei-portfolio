4b9fadc826b6887416ea269b7d51e661ab2f2e8b
Dahye <psst5491@naver.com>
2025-06-09 14:37:07 +0900

    💄(craft) move whole wavy text

--- apps/craft/src/app/wavy-text/WavyText.tsx
+++ apps/craft/src/app/wavy-text/WavyText.tsx
@@ -1,8 +1,39 @@
-import React, { ReactElement, ReactNode } from "react";
+"use client";
+
+import React, { ReactElement, ReactNode, useEffect, useRef } from "react";
+
+const TIME = 400;
+const HEIGHT = 5; // -{HEIGHT}px ~ {HEIGHT}px
 
 function WavyItem({ item }: { item: string }) {
+  const ref = useRef<HTMLSpanElement>(null);
+  const animationFrameRef = useRef<number>(null);
+
+  useEffect(() => {
+    let start = performance.now();
+
+    const animate = (time: number) => {
+      const elapsed = time - start;
+      const y = Math.sin(elapsed / TIME) * HEIGHT;
+
+      if (ref.current) {
+        ref.current.style.transform = `translateY(${y}px)`;
+      }
+
+      animationFrameRef.current = requestAnimationFrame(animate);
+    };
+
+    animationFrameRef.current = requestAnimationFrame(animate);
+
+    return () => {
+      if (animationFrameRef.current)
+        cancelAnimationFrame(animationFrameRef.current);
+    };
+  }, []);
+
   return (
     <span
+      ref={ref}
       aria-hidden="true"
       className="inline-block"
       style={{ transform: "translateY(5px)" }}
