3b49ede174e9b3bf38917605b1c0a3e255fa776a
Dahye <psst5491@naver.com>
2025-06-10 14:50:38 +0900

    💄(craft) add wavy text card

--- apps/craft/src/app/page.tsx
+++ apps/craft/src/app/page.tsx
@@ -10,12 +10,12 @@ interface Card {
 
 const ITEM_LIST = [
   {
-    title: "Debounce 예시",
+    title: "Fake 3D 알약",
     description:
-      "사용자 입력이 멈췄을 때만 이벤트를 실행하여 과도한 호출을 방지합니다.",
-    link: "/debounce",
+      "원과 사각형 요소만으로 입체적으로 회전하는 알약 애니메이션을 만듭니다.",
+    link: "/fake3dPill",
     thumbnail:
-      "https://tnzycdohhtvupgagmwfx.supabase.co/storage/v1/object/public/rhei-craft//debounce.webp",
+      "https://tnzycdohhtvupgagmwfx.supabase.co/storage/v1/object/public/rhei-craft//fake3dPill.webp",
   },
   {
     title: "Throttle 예시",
@@ -26,12 +26,19 @@ const ITEM_LIST = [
       "https://tnzycdohhtvupgagmwfx.supabase.co/storage/v1/object/public/rhei-craft//throttle.webp",
   },
   {
-    title: "Fake 3D 알약",
+    title: "Debounce 예시",
     description:
-      "원과 사각형 요소만으로 입체적으로 회전하는 알약 애니메이션을 만듭니다.",
-    link: "/fake3dPill",
+      "사용자 입력이 멈췄을 때만 이벤트를 실행하여 과도한 호출을 방지합니다.",
+    link: "/debounce",
     thumbnail:
-      "https://tnzycdohhtvupgagmwfx.supabase.co/storage/v1/object/public/rhei-craft//fake3dPill.webp",
+      "https://tnzycdohhtvupgagmwfx.supabase.co/storage/v1/object/public/rhei-craft//debounce.webp",
+  },
+  {
+    title: "Wavy Text",
+    description: "텍스트가 물결처럼 흔들리는 애니메이션을 구현합니다.",
+    link: "/wavy-text",
+    thumbnail:
+      "https://tnzycdohhtvupgagmwfx.supabase.co/storage/v1/object/public/rhei-craft//wavy-text.webp",
   },
 ];
 
@@ -65,7 +72,7 @@ export default async function Home() {
       <main className="content-x">
         <section className="mx-auto w-full max-w-6xl">
           <div className="mt-8 grid grid-cols-[1fr] gap-x-6 gap-y-12 sm:grid-cols-[1fr_1fr] sm:gap-y-8 lg:grid-cols-[1fr_1fr_1fr]">
-            {ITEM_LIST.map((item) => (
+            {ITEM_LIST.toReversed().map((item) => (
               <CardItem key={item.title} data={item} />
             ))}
           </div>
