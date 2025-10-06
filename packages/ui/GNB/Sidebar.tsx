c4febae327a62cd17ef31399796e767e33807b0f
Dahye <psst5491@naver.com>
2025-10-07 03:27:24 +0900

    💄 sidebar

--- packages/ui/GNB/Sidebar.tsx
+++ packages/ui/GNB/Sidebar.tsx
@@ -1,24 +1,17 @@
+import { motion } from "motion/react";
 import { useState } from "react";
-import { MENU } from "./menu";
 import CloseIcon from "./_assets/CloseIcon";
 import HamburgerIcon from "./_assets/HamburgerIcon";
 import ThemeButton from "./_components/ThemeButton";
+import { MENU } from "./menu";
 
 export default function Sidebar({ route }: { route: string }) {
   const [isOpen, setIsOpen] = useState(false);
 
-  function onOpen() {
-    setIsOpen(true);
-  }
-
-  function onClose() {
-    setIsOpen(false);
-  }
-
   return (
     <div className="sm:hidden relative flex gap-4">
       <ThemeButton />
-      <button onClick={onOpen}>
+      <button onClick={() => setIsOpen(true)}>
         <HamburgerIcon />
       </button>
 
@@ -26,11 +19,11 @@ export default function Sidebar({ route }: { route: string }) {
         <>
           <div
             className="fixed flex flex-col inset-0 bg-gray-900/40 z-[50]"
-            onClick={onClose}
+            onClick={() => setIsOpen(false)}
           />
-          <div className="fixed top-0 right-0 flex flex-col z-[50] bg-normal h-dvh h-full w-[max(400px,80dvw)] max-w-[100dvw] shadow-[0px_0px_32px_0px_#3363FF1F]">
+          <div className="fixed top-0 right-0 flex flex-col z-[50] bg-normal h-dvh h-full w-[max(400px,80dvw)] max-w-[100dvw] shadow-[0px_0px_32px_0px_#0263ff40] dark:shadow-[0px_0px_32px_0px_#f94b2840]">
             <div className="w-full flex justify-end">
-              <button className="p-4" onClick={onClose}>
+              <button className="p-4" onClick={() => setIsOpen(false)}>
                 <CloseIcon />
               </button>
             </div>
@@ -38,15 +31,20 @@ export default function Sidebar({ route }: { route: string }) {
             <div className="flex-1 flex flex-col">
               <ul className="flex flex-col">
                 {MENU.map((item) => (
-                  <li key={item.title} className="w-full border-b border-sub">
-                    <a
+                  <li
+                    key={item.title}
+                    className="w-full border-b border-gray-200 dark:border-gray-700"
+                  >
+                    <motion.a
                       href={item.link}
-                      className={`font-extrabold text-[1.2rem] hover:text-brand px-6 py-4 w-full inline-block ${
+                      whileHover={{ scale: 1.05 }}
+                      whileTap={{ scale: 0.95 }}
+                      className={`font-extrabold text-[1.2rem] hover:text-brand px-6 py-5 w-full inline-block ${
                         item.link === route ? "text-brand" : ""
                       }`}
                     >
                       {item.title}
-                    </a>
+                    </motion.a>
                   </li>
                 ))}
               </ul>
