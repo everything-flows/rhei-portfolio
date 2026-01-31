faebfe3d3ec3dcfb9bb9c6eeadf1d1e787ab2c2d
Dahye <psst5491@naver.com>
2026-01-31 21:51:20 +0900

    🔍(blog) 접근성 개선

--- apps/blog/app/components/TagList/index.tsx
+++ apps/blog/app/components/TagList/index.tsx
@@ -4,19 +4,21 @@ import { motion } from "motion/react";
 
 function TagItem({ item }: { item: Tag }) {
   return (
-    <Link to={`/tag/${item.id}`}>
-      <motion.button
-        whileTap={{ scale: 0.96 }}
-        whileHover={{ scale: 1.04 }}
-        className="text-brand border-brand hover:bg-brand hover:text-reverse overflow-hidden text-ellipsis whitespace-nowrap break-keep rounded-full border-[1.5px] px-2"
-      >
-        <p
-          className={`${item.isSpoiler ? "blur-[0.25rem] hover:blur-none" : ""}`}
+    <li>
+      <Link to={`/tag/${item.id}`}>
+        <motion.div
+          whileTap={{ scale: 0.96 }}
+          whileHover={{ scale: 1.04 }}
+          className="text-brand border-brand hover:bg-brand hover:text-reverse overflow-hidden text-ellipsis whitespace-nowrap break-keep rounded-full border-[1.5px] px-2"
         >
-          {item.title}
-        </p>
-      </motion.button>
-    </Link>
+          <p
+            className={`${item.isSpoiler ? "blur-[0.25rem] hover:blur-none" : ""}`}
+          >
+            {item.title}
+          </p>
+        </motion.div>
+      </Link>
+    </li>
   );
 }
 
