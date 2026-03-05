8fb914218737198a5a4a3d37c1f258bce0c208f9
Dahye <psst5491@naver.com>
2026-03-05 14:30:32 +0900

    💄(craft) update color

--- apps/craft/src/app/_components/CardItem.tsx
+++ apps/craft/src/app/_components/CardItem.tsx
@@ -41,9 +41,7 @@ export default function CardItem({ data }: { data: Card }) {
           </div>
           <div className="mt-3 px-1">
             <h3 className="text-reverse text-lg font-bold">{title}</h3>
-            <p className="mt-1 text-sm text-gray-500 dark:text-gray-400">
-              {description}
-            </p>
+            <p className="mt-1 text-sm text-gray-500">{description}</p>
           </div>
         </article>
       </Link>
