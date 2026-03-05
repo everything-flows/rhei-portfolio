0ce093cc9321cd2e832c9292d337effce6958d2d
Dahye <psst5491@naver.com>
2026-03-05 14:39:10 +0900

    🔍(blog) preload font

--- apps/blog/app/_components/Font.tsx
+++ apps/blog/app/_components/Font.tsx
@@ -2,21 +2,15 @@ export default function Font() {
   return (
     <>
       <link
-        rel="preconnect"
-        href="https://cdn.jsdelivr.net"
-        crossOrigin="anonymous"
-      />
-      <link
         rel="preload"
         as="style"
-        href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.8/dist/web/variable/pretendardvariable-dynamic-subset.css"
+        href={PRETENDARD_CSS_URL}
+        crossOrigin="anonymous"
       />
-      <noscript>
-        <link
-          rel="stylesheet"
-          href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.8/dist/web/variable/pretendardvariable-dynamic-subset.css"
-        />
-      </noscript>
+      <link rel="stylesheet" href={PRETENDARD_CSS_URL} />
     </>
   );
 }
+
+const PRETENDARD_CSS_URL =
+  "https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.8/dist/web/variable/pretendardvariable-dynamic-subset.css";
